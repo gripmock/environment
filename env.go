36f8dd40742ed674ed6175070cddfd0f91080e55
Maxim Babichev <maksim.babichev95@gmail.com>
2024-10-14 21:30:24 +0300

    stub watcher

--- env.go
+++ env.go
@@ -1,7 +1,16 @@
 package environment
 
 import (
+	"time"
+
 	env "github.com/caarlos0/env/v10"
+)
+
+type watcherType string
+
+const (
+	WatcherFSNotify watcherType = "fsnotify"
+	WatcherTimer    watcherType = "timer"
 )
 
 type Config struct {
@@ -12,14 +21,21 @@ type Config struct {
 	// Strict mode for checking the name of services and methods.
 	StrictMethodTitle bool `env:"STRICT_METHOD_TITLE" envDefault:"true"`
 
+	// GRPC server configuration.
 	GRPCNetwork string `env:"GRPC_NETWORK" envDefault:"tcp"`
 	GRPCHost    string `env:"GRPC_HOST" envDefault:"0.0.0.0"`
 	GRPCPort    string `env:"GRPC_PORT" envDefault:"4770"`
 	GRPCAddr    string `env:",expand" envDefault:"$GRPC_HOST:$GRPC_PORT"`
 
+	// HTTP server configuration.
 	HTTPHost string `env:"HTTP_HOST" envDefault:"0.0.0.0"`
 	HTTPPort string `env:"HTTP_PORT" envDefault:"4771"`
 	HTTPAddr string `env:",expand" envDefault:"$HTTP_HOST:$HTTP_PORT"`
+
+	// Files configuration.
+	StubWatcherEnabled  bool          `env:"STUB_WATCHER_ENABLED" envDefault:"false"`
+	StubWatcherInterval time.Duration `env:"STUB_WATCHER_INTERVAL" envDefault:"1s"`
+	StubWatcherType     watcherType   `env:"STUB_WATCHER_TYPE" envDefault:"fsnotify"`
 }
 
 func New() (Config, error) {
